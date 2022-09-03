91dc9587a11cc72622dd163e3282693165f2ec0f
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-09-03 09:43:36 +0530

    minner change

--- background.js
+++ background.js
@@ -1,25 +1,23 @@
-
-
-var baseURLs = {"Wikipedia": "http://en.wikipedia.org/wiki/", 
-                "Bing": "https://www.bing.com/search?q=", 
-                "Google": "https://www.google.com/search?q=",
-                 "Youtube":"https://www.youtube.com/results?search_query=",
-            };
+var baseURLs = {
+  Wikipedia: "http://en.wikipedia.org/wiki/",
+  Bing: "https://www.bing.com/search?q=",
+  Google: "https://www.google.com/search?q=",
+  Youtube: "https://www.youtube.com/results?search_query=",
+};
 
 chrome.runtime.onInstalled.addListener(() => {
-    for(let key of Object.keys(baseURLs)){
-        chrome.contextMenus.create({
-            id: key,
-            title: key,
-            type: 'normal',
-            contexts: ['selection']
-        });
-    }
-
-    chrome.contextMenus.onClicked.addListener(function(info, tab){
-        baseURL = baseURLs[info.menuItemId];
-        var newURL = baseURL + info.selectionText;
-
-        chrome.tabs.create({ url: newURL})
+  for (let key of Object.keys(baseURLs)) {
+    chrome.contextMenus.create({
+      id: key,
+      title: key,
+      type: "normal",
+      contexts: ["selection"],
     });
+  }
+
+  chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    baseURL = baseURLs[info.menuItemId];
+    var newURL = baseURL + info.selectionText;
+    chrome.tabs.create({ url: newURL });
+  });
 });
